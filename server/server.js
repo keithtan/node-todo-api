d3588e99c1d56e00ac0bb9c75c46a50e3cf48498
KEITHTAN\keithtan <keithtanhere@gmail.com>
2018-06-06 11:41:46 +0800

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,16 +1,5 @@
 require('./config/config');
 
-var env = process.env.NODE_ENV || 'development';
-console.log('env ******', env);
-
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
-
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -20,9 +9,10 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
 var app = express();
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -104,6 +94,20 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 });
 
 app.listen(port, () => {
